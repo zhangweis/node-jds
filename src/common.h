d1937bf7e11094293457aab3d67375d3cb85cc31
justmoon <justmoon@members.fsf.org>
2012-07-21 08:08:14 +0200

    Macro should not assume "use namespace v8".

--- src/common.h
+++ src/common.h
@@ -3,11 +3,11 @@
 
 #include <v8.h>
 
-#define REQ_FUN_ARG(I, VAR)                                             \
-  if (args.Length() <= (I) || !args[I]->IsFunction())                             \
-    return ThrowException(Exception::TypeError(                                   \
-                            String::New("Argument " #I " must be a function")));  \
-  Local<Function> VAR = Local<Function>::Cast(args[I]);
+#define REQ_FUN_ARG(I, VAR)                                                            \
+  if (args.Length() <= (I) || !args[I]->IsFunction())                                  \
+    return v8::ThrowException(v8::Exception::TypeError(                                \
+                              v8::String::New("Argument " #I " must be a function"))); \
+  v8::Local<v8::Function> VAR = v8::Local<v8::Function>::Cast(args[I]);
 
 static v8::Handle<v8::Value> VException(const char *msg) {
     v8::HandleScope scope;
